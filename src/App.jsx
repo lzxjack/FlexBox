dd0464b9bf9d95fa8bc007a2d653d5cbe6799951
Jack <965555169@qq.com>
2021-05-31 19:13:09 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css';
 
 const App = () => {
     return (
-        <div className="centerBox">
-            <FlexBox />
+        <div className="bg">
+            <div className="centerBox">
+                <FlexBox />
+            </div>
         </div>
     );
 };
